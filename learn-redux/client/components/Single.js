9066e65e505831f08239425319340be15d2e780c
Dillon Forestell <dillons-mac@Dillons-MacBook-Pro.local>
2019-05-24 12:29:52 -0500

    render out a single photo and create a basic comment component that will be shown next to the single

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    console.log(i);
+    const post = this.props.posts[i];
+    console.log(post);
     return (
       <div className="single-photo">
-        i am single
+        <Photo i={i} post={post} {...this.props}/>
+        <Comments/>
       </div>
     )
   }
